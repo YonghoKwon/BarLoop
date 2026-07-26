60aa3c9aee22e1fff3f423da8c0012e7b9039c26
YonghoKwon <ho2708@naver.com>
2026-07-26 10:42:34 +0900

    Refresh cache for subdivision count display

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barloop-shell-v4';
+const CACHE_NAME = 'barloop-shell-v5';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
