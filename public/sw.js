761a8c4d83770155881750905424491548c082fb
YonghoKwon <ho2708@naver.com>
2026-07-26 11:59:05 +0900

    Refresh media practice guide cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barloop-shell-v8';
+const CACHE_NAME = 'barloop-shell-v9';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
