70b0efde429ce7d5f961b70ef2ca1d719e1033ff
YonghoKwon <ho2708@naver.com>
2026-07-26 15:22:08 +0900

    Refresh iOS layout cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barloop-shell-v10';
+const CACHE_NAME = 'barloop-shell-v11';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
