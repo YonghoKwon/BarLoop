62fd8da59fecaf0dd83ebd5187a6e0a3da1d2684
YonghoKwon <ho2708@naver.com>
2026-07-26 11:07:31 +0900

    Refresh visual grid cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barloop-shell-v6';
+const CACHE_NAME = 'barloop-shell-v7';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
