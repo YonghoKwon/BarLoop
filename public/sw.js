859bab5c080e88e5e1e588e51b6cef7c915b9d81
YonghoKwon <ho2708@naver.com>
2026-07-26 23:53:27 +0900

    Refresh PWA cache for accent controls

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barloop-shell-v17';
+const CACHE_NAME = 'barloop-shell-v18';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
