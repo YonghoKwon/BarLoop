5283a04d314beb5dcd537b1426e16cfe11a11372
YonghoKwon <ho2708@naver.com>
2026-07-27 00:04:12 +0900

    Refresh PWA cache after UI fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barloop-shell-v18';
+const CACHE_NAME = 'barloop-shell-v19';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
